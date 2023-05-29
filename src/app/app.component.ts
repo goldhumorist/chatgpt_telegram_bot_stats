1af0890bd817252de7c11bb10ca2bc9b5ba0c60f
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-05-29 09:51:28 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chatgpt_telegram_bot_stats_frontend';
+}
