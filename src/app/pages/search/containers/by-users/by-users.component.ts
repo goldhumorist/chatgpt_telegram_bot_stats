01310dd4148a948089629f72259fe51573464cca
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-06-04 20:12:55 +0200

    feature: Add search-by-username page

--- src/app/pages/search/containers/by-users/by-users.component.ts
+++ src/app/pages/search/containers/by-users/by-users.component.ts
@@ -1,4 +1,9 @@
+import { BehaviorSubject, Observable, finalize, shareReplay, tap } from 'rxjs';
+import { ISearchByUsernameResponse } from './../../../../features/interfaces';
+import { SearchByUsernameService } from './../../../../features/search/api/seach-by-username.service';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SearchRoutingEnum } from '../../enums';
+import { TSearchByUsernameData } from '../../interfaces';
 
 @Component({
   selector: 'app-by-users',
@@ -6,4 +11,92 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./by-users.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ByUsersComponent {}
+export class ByUsersComponent {
+  readonly searchIndexPageRoute = SearchRoutingEnum.SearchIndexPage;
+
+  constructor(private searchByUsernameService: SearchByUsernameService) {}
+
+  searchResponse$!: Observable<ISearchByUsernameResponse>;
+  paginationData = {
+    page: 1,
+    limit: 5,
+    totalAvaibleAmount: 0,
+    currentAmount: 0,
+  };
+  tempSearchData!: TSearchByUsernameData;
+  isPreviousPageAvaible = false;
+  isNextPageAvaible = true;
+  isLoading$ = new BehaviorSubject<boolean>(false);
+
+  onSearchSubmit(data: TSearchByUsernameData) {
+    this.tempSearchData = data;
+    this.isLoading$.next(true);
+
+    this.searchResponse$ = this.searchByUsernameService
+      .search({ ...this.tempSearchData, ...this.paginationData })
+      .pipe(
+        tap(data => {
+          this.paginationData.totalAvaibleAmount = data.total.value;
+          this.paginationData.currentAmount += data.hits.length;
+        }),
+        finalize(() => this.isLoading$.next(false)),
+        shareReplay(1)
+      );
+  }
+
+  loadNextPage() {
+    if (
+      this.paginationData.totalAvaibleAmount > this.paginationData.currentAmount
+    ) {
+      this.isPreviousPageAvaible = true;
+      this.paginationData.page += 1;
+      this.isLoading$.next(true);
+
+      this.searchResponse$ = this.searchByUsernameService
+        .search({ ...this.tempSearchData, ...this.paginationData })
+        .pipe(
+          tap(data => {
+            this.paginationData.currentAmount += data.hits.length;
+          }),
+          finalize(() => this.isLoading$.next(false)),
+
+          shareReplay(1)
+        );
+
+      if (
+        this.paginationData.totalAvaibleAmount <=
+        this.paginationData.currentAmount + this.paginationData.limit
+      )
+        this.isNextPageAvaible = false;
+
+      return;
+    }
+    this.isNextPageAvaible = false;
+  }
+
+  loadPreviousPage() {
+    if (this.paginationData.page > 1) {
+      this.paginationData.page -= 1;
+      this.isLoading$.next(true);
+
+      this.searchResponse$ = this.searchByUsernameService
+        .search({ ...this.tempSearchData, ...this.paginationData })
+        .pipe(
+          tap(data => {
+            this.paginationData.currentAmount -= data.hits.length;
+          }),
+          finalize(() => this.isLoading$.next(false)),
+
+          shareReplay(1)
+        );
+
+      this.isNextPageAvaible = true;
+
+      if (this.paginationData.page === 1) this.isPreviousPageAvaible = false;
+
+      return;
+    }
+
+    this.isPreviousPageAvaible = false;
+  }
+}
--- src/app/pages/search/containers/by-users/by-users.module.ts
+++ src/app/pages/search/containers/by-users/by-users.module.ts
@@ -1,3 +1,5 @@
+import { FeaturesModule } from './../../../../features/features.module';
+import { SharedModule } from './../../../../shared/shared.module';
 import { ByUsersRouting } from './by-users.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,6 +7,6 @@ import { ByUsersComponent } from './by-users.component';
 
 @NgModule({
   declarations: [ByUsersComponent],
-  imports: [CommonModule, ByUsersRouting],
+  imports: [CommonModule, ByUsersRouting, SharedModule, FeaturesModule],
 })
 export class ByUsersModule {}
