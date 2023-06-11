95bbc34eb5cb308c10982b77e2857e228a9304dd
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-06-11 14:23:10 +0200

    fix: Change detectionStrategy in LoadingSpinerComponent

--- src/app/shared/components/loading-spiner/loading-spiner.component.ts
+++ src/app/shared/components/loading-spiner/loading-spiner.component.ts
@@ -1,10 +1,11 @@
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spiner',
   templateUrl: './loading-spiner.component.html',
   styleUrls: ['./loading-spiner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoadingSpinerComponent {
   @Input()
