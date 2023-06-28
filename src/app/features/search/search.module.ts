73e7a62c8c7d94ce94a544320e450ca5d33e7fac
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-06-28 17:58:38 +0200

    refactor: Delete unnecessary components

--- src/app/features/search/search.module.ts
+++ src/app/features/search/search.module.ts
@@ -1,9 +1,8 @@
 import { ByUserSearchFormModule } from './by-user-search-form';
 import { FullTextSearchFormModule } from './full-text-search-form';
 import { NgModule } from '@angular/core';
-import { SearchFormModule } from './search-form';
 
 @NgModule({
-  exports: [SearchFormModule, FullTextSearchFormModule, ByUserSearchFormModule],
+  exports: [FullTextSearchFormModule, ByUserSearchFormModule],
 })
 export class SearchModule {}
--- src/app/features/statistic/statistic.module.ts
+++ src/app/features/statistic/statistic.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { UsersStatisticFormModule } from './users-statistic-form';
 import { UsageStatisticFormModule } from './usage-statistic-form';
-import { StatisticFormModule } from './statistic-form';
 
 @NgModule({
-  exports: [
-    StatisticFormModule,
-    UsersStatisticFormModule,
-    UsageStatisticFormModule,
-  ],
+  exports: [UsersStatisticFormModule, UsageStatisticFormModule],
 })
 export class StatisticModule {}
